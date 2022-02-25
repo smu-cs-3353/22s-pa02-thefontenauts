4afe20266c54c1b35416e56e15284cf2a4c52e62
Rds1055 <rdschaefer@mail.smu.edu>
2022-02-25 14:00:20 -0600

    Created file manager class

---
+++ src/FileManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ryan Schaefer on 2/25/22.
+//
+
+#include "FileManager.h"
---
+++ src/FileManager.h
@@ -0,0 +1,19 @@
+#ifndef PA02_FILEMANAGER_H
+#define PA02_FILEMANAGER_H
+
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class FileManager {
+private:
+    vector<string> files;
+    vector<string> sList;
+    vector<int> iList;
+public:
+    FileManager() = default;
+
+};
+
+
+#endif //PA02_FILEMANAGER_H
