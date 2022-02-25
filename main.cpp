181770de9d04828a909e4f99551f328125b2fe80
Rds1055 <rdschaefer@mail.smu.edu>
2022-02-25 11:03:47 -0600

    Basic setup

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/* Wes Anderson and Ryan Schaefer
+ * 22s-CS-3353 PA 2
+ * 2/8/22
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    cout << "Segmentation fault" << endl;
+    return 1;
+}
